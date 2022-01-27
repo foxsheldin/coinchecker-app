729012331b4b7fe85b04c6ad0ed6615be7abfb09
foxsheldin <fruktsky@gmail.com>
2022-01-27 17:17:26 +0300

    Выделение header() в отдельный файл

---
+++ server.cc.php/api/header.php
@@ -0,0 +1,7 @@
+<?
+    header('Access-Control-Allow-Origin: http://localhost:3000');
+    header("Access-Control-Allow-Credentials: true");
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+    header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
+    
+?>
