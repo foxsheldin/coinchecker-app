4fb7d24354f078e20b4aed3d6de56c1414888514
foxsheldin <fruktsky@gmail.com>
2022-01-28 07:42:35 +0300

    Добавлен HOC withUserID

---
+++ src/hoc/withUserID.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+export const withUserID = (Component) => {
+    const IntegratedComponent = (props) => {
+        /* let navigate = useNavigate();
+        if (props.userid==null) navigate("/auth"); */
+        return <Component {...props} userid={props.userid} />
+    }
+
+    const mapStateToProps = (state) => {
+        return {
+            userid: state.auth.userid
+        }
+    } 
+    return connect(mapStateToProps)(IntegratedComponent);
+}
